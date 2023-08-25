1f2f42598a38c6aae672d12906c8685b95e3ab3e
gmgabrie <ggabriel420@gmail.com>
2023-08-25 16:11:08 -0400

    added two projects to portfolio page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,7 +6,11 @@ function About() {
     <section className="my-5">
       <div className="my-2">
         <div className="profile-img my-5">
-          <img class="aboutPic" src={profileImage} alt="profile image"></img>
+          <img
+            className="aboutPic"
+            src={profileImage}
+            alt="profile image"
+          ></img>
           {/* <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i> */}
         </div>
         <p>
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -5,29 +5,29 @@ function Portfolio() {
   // Replace links with deployed projects and GitHub repos
   const [projects] = useState([
     {
-      name: "project-1",
-      description: "description",
-      link: "link",
-      repo: "link",
+      name: "Gulpco",
+      description: "MERN Full Stack E-Commerce",
+      link: "https://gulpcodrinks-ad436ff6ec24.herokuapp.com/",
+      repo: "https://github.com/josejrod07/ecommerce-project3",
     },
     {
-      name: "project-2",
-      description: "description",
-      link: "link",
-      repo: "link",
+      name: "WeatherWizard",
+      description: "Server-side API",
+      link: "https://gmgabrie.github.io/weather-wizard/",
+      repo: "https://github.com/gmgabrie/weather-wizard",
     },
-    {
-      name: "project-3",
-      description: "description",
-      link: "link",
-      repo: "link",
-    },
-    {
-      name: "project-4",
-      description: "description",
-      link: "link",
-      repo: "link",
-    },
+    // {
+    //   name: "project-3",
+    //   description: "description",
+    //   link: "link",
+    //   repo: "link",
+    // },
+    // {
+    //   name: "project-4",
+    //   description: "description",
+    //   link: "link",
+    //   repo: "link",
+    // },
   ]);
 
   return (
